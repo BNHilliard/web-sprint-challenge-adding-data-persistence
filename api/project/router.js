3325c7ec081478466afcaef110eee2f2f950e58c
timothy hoover <bnhilliard>
2022-12-01 21:10:39 -0800

    work on migration in the morning

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
 // build your `/api/projects` router here
+const router = require('express').Router();
+const Project = require('./model');
+
+
+router.get('/', (req, res, next) => {
+    Project.getProjects()
+    .then(resp =>{
+        res.json(resp)
+    }).catch(next)
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "something went wrong in the project router",
+        stack: err.stack
+    })
+})
+
+module.exports = router
---
+++ data/migrations/20221202045929_01-project.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+  return knex.scheme
+  .createTable('', tbl => {})
+  .createTable('', tbl => {})
+  .createTable('', tbl => {})
+  .createTable('', tbl => {})
+};
+
+
+exports.down = function(knex) {
+  
+};
